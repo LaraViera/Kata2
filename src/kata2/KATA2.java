a4d1909435799408d734a875a7db0eeb8b0c1cd7
la_mi <la_mi@DESKTOP-0SC39IM.home>
2017-10-18 19:42:23 +0100

    Tercera Versión KATA 2.
    Se define la clase Histogram

--- src/kata2/KATA2.java
+++ src/kata2/KATA2.java
@@ -7,24 +7,13 @@ public class KATA2 {
 
     public static void main(String[] args) {
         int[] data = {1, 1, 1, 0, 3, 4, 4, 3, 0, 0, 1, 1, 1, 1, 3, 2};
-        Map<Integer, Integer> histogram = new HashMap<>();
+        Histogram histo = new Histogram(data);
+        Map<Integer, Integer> histogr = histo.getHistogram();
 
-        for (int i = 0; i < data.length; i++) {
-            if (histogram.containsKey(data[i])) {
-                histogram.put(data[i], histogram.get(data[i]) + 1);
-            } else {
-                histogram.put(data[i], 1);
-            }
-        }
-
-        for (int key : data) {
-            histogram.put(key, histogram.containsKey(key) ?
-                    histogram.get(key) + 1 : 1);
-        }
-        
-        for (int key : histogram.keySet()) {
+       
+        for (int key : histogr.keySet()) {
             System.out.println("El número " + key + " aparece "
-                    + histogram.get(key) + " veces");
+                    + histogr.get(key) + " veces");
         }
     }
 }
